c96dba49278112f5f192d280c29bd67cbb7c8111
chengliang-lim <offchengliang@gmail.com>
2021-10-06 20:11:25 +0800

    Removing body-parser library
    Remove body parser lib

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const helmet = require('helmet');
 const cors = require('cors');
 
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+
 app.use(helmet());
 app.use(cors());
 
