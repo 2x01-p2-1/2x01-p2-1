6104858d239f8aaa055404d68185e2c918fe956e
chengliang-lim <offchengliang@gmail.com>
2021-12-02 22:42:28 +0800

    TCP Functions

--- ZoomAway/server.js
+++ ZoomAway/server.js
@@ -5,6 +5,33 @@ const mongoose = require('mongoose');
 const session = require("express-session");
 const passport = require("passport");
 var cookieParser = require('cookie-parser')
+const net = require('net')
+const fs = require('fs')
+
+const tcpServer = net.createServer(function (client) {
+  console.log("MSP432 Is Connected")
+  client.setEncoding('utf-8')
+  client.on('data', function (data) {
+    console.log(data)
+    if (data == "Hello") {
+      try {
+        const data = fs.readFileSync('MSP432 Files/commands.txt', 'utf8')
+        fs.truncateSync( 'MSP432 Files/commands.txt', 0 )
+        client.end("FLRFLR")
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
+      // console.log(JSON.parse(data))
+
+    }
+  })
+  client.on('end', function () {
+    console.log("MSP432 Disconnected")
+  })
+
+});
+tcpServer.listen(5000)
 
 
 const app = express();
@@ -45,7 +72,7 @@ app.get('/', function (req, res) {
     res.send('Welcome to SIT.')
 });
 
-app.listen(port,host, () => {
+app.listen(3000, () => {
   // print a message when the server starts listening
   console.log("server starting on " + host + ":" + port);
 });
